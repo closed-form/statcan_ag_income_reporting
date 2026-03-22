fb65807d228792ada325b1709fe7c782b8bf18d4
unknown <auldbenjamin94@gmail.com>
2026-03-22 11:56:19 -0400

    Moved the data grabbing and filtering to another file which will do that, then render the markdown which loads a separate RData file

---
+++ prep_and_knit_atdp_report.R
@@ -0,0 +1,128 @@
+#### Import Packages ####
+
+present_packages<-as.data.frame(installed.packages())$Package
+
+weird_packages<-c("vctrs",
+                  "xfun")
+
+needed_weird_packages<-setdiff(weird_packages,
+                               present_packages)
+
+if(length(needed_weird_packages)>0){
+  for(i in 1:length(needed_weird_packages)){
+    install.packages(needed_weird_packages[i],
+                     dependencies = TRUE)
+  }
+  rm(i)
+}
+
+required_packages<-c("tidyverse",
+                     "devtools",
+                     "tinytex",
+                     "kableExtra",
+                     "lubridate",
+                     "stringr",
+                     "remotes")
+
+needed_packages<-setdiff(required_packages,
+                         present_packages)
+
+if(length(needed_packages)>0){
+  for(i in 1:length(needed_packages)){
+    install.packages(needed_packages[i],
+                     dependencies = TRUE)
+  }
+  rm(i)
+}
+
+if(sum(as.numeric(present_packages=="qs"))==0){
+  remotes::install_version("qs", version = "0.27.3")
+}
+
+if(sum(as.numeric(present_packages=="statcanR"))==0){
+  pak::pak('warint/statcanR')
+}
+
+library(tidyverse)
+library(devtools)
+library(tinytex)
+library(kableExtra)
+library(lubridate)
+library(stringr)
+library(statcanR)
+
+rm(present_packages,
+   weird_packages,
+   needed_weird_packages,
+   required_packages,
+   needed_packages)
+
+#### Import Data ####
+
+atdp<-statcan_data(tableNumber = "32-10-0136-01",
+                   lang = "eng") %>%
+  filter(!is.na(VALUE))
+
+estimate_types<-c("Total estimate",
+                  "Average per farm")
+
+vars<-atdp %>%
+  filter(GEO=="Canada",
+         `Farm type`=="All farm types",
+         `Revenue class`=="All revenue classes",
+         `Estimate type` %in% estimate_types) %>%
+  select(Estimates) %>%
+  distinct()
+
+vars<-as.vector(unlist(vars))
+
+selected_vars<-select.list(
+  choices = vars,
+  title = paste0("Select vars to summarize. ",
+                 "Ctrl+click to multi-select"),
+  multiple = TRUE,
+  graphics = TRUE)
+
+selected_type<-select.list(
+  choices = estimate_types,
+  title = "Select ONE estimate type.",
+  multiple = FALSE,
+  graphics = TRUE)
+
+selected_prov<-select.list(
+  choices = unique(atdp$GEO),
+  title = "Select ONE province.",
+  multiple = FALSE,
+  graphics = TRUE)
+
+selected_atdp<-atdp %>%
+  filter(Estimates %in% selected_vars,
+         GEO==selected_prov,
+         `Estimate type`==selected_type,
+         `Farm type`=="All farm types",
+         `Revenue class`=="All revenue classes")
+
+rm(estimate_types,
+   vars)
+
+save(selected_atdp,
+     selected_vars,
+     selected_prov,
+     selected_type,
+     file = "atdp_data.RData")
+
+rm(selected_atdp,
+   selected_vars,
+   selected_prov,
+   selected_type)
+
+#### Render Markdown Doc ####
+
+rmarkdown::render(
+  input = "atdp_report.Rmd",
+  output_format = "pdf_document",
+  output_file = paste0("atdp_report_",
+                       Sys.getenv('USERNAME'),"_",
+                       format(Sys.time(),
+                              '%Y-%m-%d_%H-%M-%S'),
+                       ".pdf"))
